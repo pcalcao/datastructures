5e50d2e8b8f32c03660ad99e443e4cedf9b8efe6
pcalcao <pedro.calcao@gmail.com>
2012-08-31 18:29:42 +0100

    Refactored class name, added search method.

--- binary_tree.py
+++ binary_tree.py
@@ -1,21 +1,40 @@
 #!/etc/python
 
-class BinaryTree():
+class BinarySearchTree():
     def __init__(self):
         self.root = None
         self.left = None
         self.right = None
+        self.parent = None
 
     def put(self, value):
         if self.root is None:
             self.root = value
         elif value < self.root:
             if self.left is None:
-                self.left = BinaryTree()
+                self.left = BinarySearchTree()
             self.left.put(value)
+            self.left.parent = self
         elif value > self.root:
-            self.right = BinaryTree()
+            self.right = BinarySearchTree()
             self.right.put(value)
+            self.right.parent = self
+
+    def search(self, value):
+        if value  == self.root:
+            return self
+
+        if value < self.root and self.left is not None:
+            return self.left.search(value)
+
+        if value > self.root and self.right is not None:
+            return self.right.search(value)
+
+        return None
+
+    def delete(self, value):
+        pass
+
 
 
 
--- tests/test_binary_tree.py
+++ tests/test_binary_tree.py
@@ -1,27 +1,43 @@
 __author__ = 'pcalcao'
 
 import unittest
-from binary_tree import BinaryTree
+from binary_tree import BinarySearchTree
 
 
 class TestBinaryTree(unittest.TestCase):
 
     def test_creation(self):
-        bTree = BinaryTree()
+        bTree = BinarySearchTree()
         bTree.put(1)
         self.assertEqual(1, bTree.root)
-        bTree = BinaryTree()
+        bTree = BinarySearchTree()
         bTree.put(2)
         self.assertEqual(2, bTree.root)
 
     def test_ad_values_left_and_right(self):
-        bTree = BinaryTree()
+        bTree = BinarySearchTree()
         bTree.put(2)
         self.assertEqual(2, bTree.root)
         bTree.put(1)
         self.assertEqual(1, bTree.left.root)
+        self.assertEqual(bTree, bTree.left.parent)
         bTree.put(3)
         self.assertEqual(3, bTree.right.root)
+        self.assertEqual(bTree, bTree.right.parent)
+
+
+    def test_search(self):
+        bTree = BinarySearchTree()
+        bTree.put(2)
+        bTree.put(1)
+        bTree.put(3)
+
+        self.assertEqual(bTree.search(2), bTree)
+        self.assertEqual(bTree.search(1), bTree.left)
+        self.assertEqual(bTree.search(3), bTree.right)
+
+        self.assertIsNone(bTree.search(4))
+
 
 
 
