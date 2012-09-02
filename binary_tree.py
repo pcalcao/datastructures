8b16a70fae4403f613281419d414974f9830ec51
pcalcao <pedro.calcao@gmail.com>
2012-09-02 23:58:39 +0100

    Added delete method for leafs only

--- binary_tree.py
+++ binary_tree.py
@@ -2,38 +2,52 @@
 
 class BinarySearchTree():
     def __init__(self):
-        self.root = None
+        self.value = None
         self.left = None
         self.right = None
         self.parent = None
 
     def put(self, value):
-        if self.root is None:
-            self.root = value
-        elif value < self.root:
+        if self.value is None:
+            self.value = value
+        elif value < self.value:
             if self.left is None:
                 self.left = BinarySearchTree()
             self.left.put(value)
             self.left.parent = self
-        elif value > self.root:
+        elif value > self.value:
             self.right = BinarySearchTree()
             self.right.put(value)
             self.right.parent = self
 
     def search(self, value):
-        if value  == self.root:
+        if value  == self.value:
             return self
 
-        if value < self.root and self.left is not None:
+        if value < self.value and self.left is not None:
             return self.left.search(value)
 
-        if value > self.root and self.right is not None:
+        if value > self.value and self.right is not None:
             return self.right.search(value)
 
         return None
 
     def delete(self, value):
-        pass
+        #If I'm a leaf and you want to delete me (shame on you)
+        if self.value == value and self.is_leaf:
+            if self.parent.value < self.value: #I'm the right node
+                self.parent.right = None
+                self.parent = None
+            elif self.parent.value > self.value: #I'm the left node
+                self.parent.left = None
+                self.parent = None
+        elif value > self.value and self.right is not None:
+            self.right.delete(value)
+        elif  value < self.value and self.left is not None:
+            self.left.delete(value)
+
+    def is_leaf(self):
+        return self.left is None and self.right is None
 
 
 
--- tests/test_binary_tree.py
+++ tests/test_binary_tree.py
@@ -9,34 +9,53 @@ class TestBinaryTree(unittest.TestCase):
     def test_creation(self):
         bTree = BinarySearchTree()
         bTree.put(1)
-        self.assertEqual(1, bTree.root)
+        self.assertEqual(1, bTree.value)
         bTree = BinarySearchTree()
         bTree.put(2)
-        self.assertEqual(2, bTree.root)
+        self.assertEqual(2, bTree.value)
 
     def test_ad_values_left_and_right(self):
         bTree = BinarySearchTree()
         bTree.put(2)
-        self.assertEqual(2, bTree.root)
+        self.assertEqual(2, bTree.value)
         bTree.put(1)
-        self.assertEqual(1, bTree.left.root)
+        self.assertEqual(1, bTree.left.value)
         self.assertEqual(bTree, bTree.left.parent)
         bTree.put(3)
-        self.assertEqual(3, bTree.right.root)
+        self.assertEqual(3, bTree.right.value)
         self.assertEqual(bTree, bTree.right.parent)
 
 
     def test_search(self):
-        bTree = BinarySearchTree()
-        bTree.put(2)
-        bTree.put(1)
-        bTree.put(3)
-
+        bTree = init_tree()
         self.assertEqual(bTree.search(2), bTree)
         self.assertEqual(bTree.search(1), bTree.left)
+        self.assertEqual(bTree.search(1).parent, bTree)
         self.assertEqual(bTree.search(3), bTree.right)
-
+        self.assertEqual(bTree.search(3).parent, bTree)
         self.assertIsNone(bTree.search(4))
+
+
+    def test_delete_left(self):
+        bTree = init_tree()
+        self.assertEqual(bTree.search(1), bTree.left)
+        bTree.delete(1)
+        self.assertIsNone(bTree.search(1))
+
+    def test_delete_right(self):
+        bTree = init_tree()
+        self.assertEqual(bTree.search(3), bTree.right)
+        bTree.delete(3)
+        #self.assertIsNone(bTree.search(3))
+
+
+def init_tree():
+    bTree = BinarySearchTree()
+    bTree.put(2)
+    bTree.put(1)
+    bTree.put(3)
+    return bTree
+
 
 
 
